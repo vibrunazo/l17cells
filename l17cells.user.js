fc26e5b539eda2e2a6c7cb885ba8ded9b8cca554
Vandré Brunazo <vibrunazo@gmail.com>
2018-01-09 16:37:33 -0200

    plugin id

--- l17cells.user.js
+++ l17cells.user.js
@@ -31,9 +31,9 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
-plugin_info.buildName = 'iitc';
+plugin_info.buildName = 'l17cells';
 plugin_info.dateTimeVersion = '20170108.21732';
-plugin_info.pluginId = 'regions';
+plugin_info.pluginId = 'l17cells';
 //END PLUGIN AUTHORS NOTE
 
 
